29aac4a16a71045f7d5d9ed53b75bf79ecb7da1d
Alex <59831659+alt-ee@users.noreply.github.com>
2025-07-11 11:37:31 +0100

    tmux changes even tho im not using it atm

--- home/alex/features/cli/tmux.nix
+++ home/alex/features/cli/tmux.nix
@@ -15,8 +15,27 @@
     terminal = "screen-256color";
 
     extraConfig = ''
-      unbind-key -a
-      bind-key r source-file ${config.xdg.configHome}/tmux/tmux.conf
+      unbind -a
+      bind r source-file ${config.xdg.configHome}/tmux/tmux.conf
+      bind s split-window -h
+      bind d split-window -v
+      bind x kill-pane
+      bind z detach-client
+      bind l choose-tree -s
+      bind , command-prompt -I'#W' { rename-window -- '%%' }
+
+      bind -n M-\; command-prompt
+
+      bind -n M-h select-pane -L
+      bind -n M-l select-pane -R
+      bind -n M-k select-pane -U
+      bind -n M-j select-pane -D
+
+      bind -n M-\[ previous-window
+      bind -n M-\] next-window
+
+      set -g mouse on
+      set -g allow-rename off
     '';
   };
 }
